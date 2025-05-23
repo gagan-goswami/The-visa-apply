6a4b1f8b6741aa2b3a3454b2182734777acd49e7
gagan-goswami <gagan678go@gmail.com>
2025-05-23 11:00:08 +0530

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,20 +153,19 @@ AOS.init();
 
 $(document).ready(function () {
   $(".counter-value").each(function () {
-    $(this)
-      .prop("Counter", 0)
-      .animate(
-        {
-          Counter: $(this).text(),
+    var $this = $(this),
+      countTo = $this.attr("data-count");
+
+    $({ Counter: 0 }).animate(
+      { Counter: countTo },
+      {
+        duration: 5000,
+        easing: "swing",
+        step: function (now) {
+          $this.text(Math.ceil(now) + "+");
         },
-        {
-          duration: 5000,
-          easing: "swing",
-          step: function (now) {
-            $(this).text(Math.ceil(now));
-          },
-        }
-      );
+      }
+    );
   });
 });
 
